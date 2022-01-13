7965539671dcc9c1e391704d7f92cdc61fc4d059
Diana Stralberg <stralber@ualberta.ca>
2022-01-13 09:11:06 -0700

    Update BCR6ModelsBeaudoinLandscapeClimatePredict.R

--- BCR6ModelsBeaudoinLandscapeClimatePredict.R
+++ BCR6ModelsBeaudoinLandscapeClimatePredict.R
@@ -36,7 +36,10 @@ get_cn <- function(z, rmax=0.9) {
   return(vars)
 }
 
+varimpclasses <- read.csv("G:/Boreal/NationalModelsV2/BCR6/varimpclasses.csv")
+
 bluegreen.colors <- colorRampPalette(c("#FFF68F", "khaki1","#ADFF2F", "greenyellow", "#00CD00", "green3", "#48D1CC", "mediumturquoise", "#007FFF", "blue"), space="Lab", bias=0.8)
+prov <- rgdal::readOGR("E:/GIS/basemaps/province_state_LCC.shp")
 provstate <- rgdal::readOGR("E:/GIS/basemaps/province_state_line.shp")
 LCC <- CRS("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs")
 bcr6 <- shapefile("G:/Boreal/NationalModelsV2/BCR6/bcr6.shp")
@@ -120,17 +123,21 @@ survey2011 <- aggregate(PC2011$ABUND, by=list("PKEY"=PC2011$PKEY,"SS"=PC2011$SS,
 # survey2011 <- survey2011[sample(1:nrow(survey2011)), ]
 survey <- rbind(survey2001,survey2011) #n=126402
 
+surveyXY <- dplyr::left_join(survey,cdat[,1:3],by=c("SS"))
+coordinates(surveyXY) <- c("X","Y")
+surveyXY$prov <- cbind(surveyXY,raster::extract(prov,surveyXY))
+
 #calculating sample weights as inverse of survey effort within 5x5 pixel area
 samprast2011 <- rasterize(cbind(dat2011$X,dat2011$Y), r2, field=1, fun='sum')
 sampsum25 <- focal(samprast2011, w=matrix(1,nrow=5,ncol=5), na.rm=TRUE)
-dat_2011 <- cbind(dat2011,extract(sampsum25,as.matrix(cbind(dat2011$X,dat2011$Y))))
+dat_2011 <- cbind(dat2011,raster::extract(sampsum25,as.matrix(cbind(dat2011$X,dat2011$Y))))
 names(dat_2011)[ncol(dat_2011)] <- "sampsum25"
 dat_2011$wt <- 1/dat_2011$sampsum25
 dat_2011$SS <- as.character(dat_2011$SS) #n=34022
 
 samprast2001 <- rasterize(cbind(dat2001$X,dat2001$Y), r2, field=1, fun='sum')
 sampsum25 <- focal(samprast2001, w=matrix(1,nrow=5,ncol=5), na.rm=TRUE)
-dat_2001 <- cbind(dat2001,extract(sampsum25,as.matrix(cbind(dat2001$X,dat2001$Y))))
+dat_2001 <- cbind(dat2001,raster::extract(sampsum25,as.matrix(cbind(dat2001$X,dat2001$Y))))
 names(dat_2001)[ncol(dat_2001)] <- "sampsum25"
 dat_2001$wt <- 1/dat_2001$sampsum25
 dat_2001$SS <- as.character(dat_2001$SS) #n=18837
@@ -208,19 +215,71 @@ mapplot <- function (j) {
   dev.off()
 }
 
-cvstatsum <- function (speclist) {
-  cvstats <- read.csv(paste(w,speclist[1],"cvstats8.csv",sep=""))
-  cvstatmean <- as.data.frame(cbind(as.character(cvstats[,1]),rowMeans(cvstats[,2:6])))
-  names(cvstatmean) <- c("stat",as.character(speclist[1]))
-  for (j in 2:length(speclist)) {
-    x<-try(cv2 <- read.csv(paste(w,speclist[j],"cvstats8.csv",sep="")))
-    if(class(x) != "try-error") {
-      cvstatmean <- as.data.frame(cbind(cvstatmean,rowMeans(cv2[,2:6])))
-      names(cvstatmean)[ncol(cvstatmean)] <- as.character(speclist[j])
+prev <- function (speclist) {
+  rast <- raster(paste(w,speclist[1],"_pred1km8.tif",sep=""))
+  prev <- cellStats(rast, 'mean')	
+  med <- quantile(rast, probs = c(0.5))
+  prevtab <- data.frame("spec"=speclist[1],"meandens"=prev,"meddens"=as.numeric(med))
+  for (j in 2:length(speclist)){
+    x1 <- try(rast<-raster(paste(w,speclist[j],"_pred1km8.tif",sep="")))
+    if(class(x1) != "try-error") {
+      prev <- cellStats(rast, 'mean')	
+      med <- quantile(rast, probs = c(0.5))
+      prevtab1 <- data.frame("spec"=speclist[j],"meandens"=prev,"meddens"=as.numeric(med))
+      prevtab <- rbind(prevtab,prevtab1)
     }
   }
-  return(cvstatmean)
+  return(prevtab)
 }
+
+# cvstatsum <- function (speclist) {
+#   cvstats <- read.csv(paste(w,speclist[1],"cvstats8.csv",sep=""))
+#   cvstatmean <- as.data.frame(cbind(as.character(cvstats[,1]),rowMeans(cvstats[,2:5])))
+#   names(cvstatmean) <- c("stat",as.character(speclist[1]))
+#   for (j in 2:length(speclist)) {
+#     x<-try(cv2 <- read.csv(paste(w,speclist[j],"cvstats8.csv",sep="")))
+#     if(class(x) != "try-error") {
+#       cvstatmean <- as.data.frame(cbind(cvstatmean,rowMeans(cv2[,2:5])))
+#       names(cvstatmean)[ncol(cvstatmean)] <- as.character(speclist[j])
+#     }
+#   }
+#   return(cvstatmean)
+# }
+
+cvstats2 <- function (speclist) {
+  x <- try(load(paste(w,speclist[1],"brt8.R",sep="")))
+  varimp <- read.csv(paste(w,speclist[1],"varimp8.csv",sep=""))
+  cvstats <- as.data.frame(brt1$cv.statistics[c(1,3)])
+  cvstats$deviance.null <- brt1$self.statistics$mean.null
+  cvstats$deviance.exp <- (cvstats$deviance.null-cvstats$deviance.mean)/cvstats$deviance.null
+  cvstats$SPEC <- speclist[1]
+  for (j in 2:length(speclist)) {
+    x <- try(load(paste(w,speclist[j],"brt8.R",sep="")))
+    if(class(x)!="try-error"){
+      varimp <- read.csv(paste(w,speclist[j],"varimp8.csv",sep=""))
+      cvstats1 <- as.data.frame(brt1$cv.statistics[c(1,3)])
+      cvstats1$deviance.null <- brt1$self.statistics$mean.null
+      cvstats1$deviance.exp <- (cvstats1$deviance.null-cvstats1$deviance.mean)/cvstats1$deviance.null
+      cvstats1$SPEC <- speclist[j]
+      cvstats <- rbind(cvstats,cvstats1)
+    }
+  }
+  return(cvstats)
+}
+
+# varimpsum <- function (speclist) {
+#   varimp <- read.csv(paste(w,speclist[1],"varimp8.csv",sep=""))
+#   varimp$SPEC <- speclist[1]
+#   for (j in 2:length(speclist)) {
+#     x<-try(varimp1 <- read.csv(paste(w,speclist[j],"varimp8.csv",sep="")))
+#     if(class(x)!="try-error"){
+#       varimp1$SPEC <- speclist[j]
+#       varimp <- rbind(varimp,varimp1)
+#     }
+#   }
+#   varimp <- merge(varimp,varimpclasses,by="var")
+#   return(varimp)
+# }
 
 for (j in 1:length(speclist)) {
   x<-try(rast <- raster(paste(w,speclist[j],"_pred1km8.tif",sep="")))
@@ -301,5 +360,15 @@ for (j in 1:length(speclist)) {
   }
 }
 
-cvstats <- cvstatsum(speclist)
+prevtab <- prev(speclist)
+write.csv(prevtab,file=paste(w,"_prevtab8.csv",sep=""))
+
+cvstats <- cvstats2(speclist)
 write.csv(cvstats,file=paste(w,"_cvstats8.csv",sep=""))
+
+# varimp <- varimpsum(speclist)
+# varimpsummary <- aggregate(varimp[,3],by=list(varimp$SPEC,varimp$class),FUN=sum)
+# names(varimpsummary)<- c("SPEC","varclass","rel.inf")
+# varimpwide <- dcast(varimpsummary, SPEC ~ varclass)
+# statscombo <- merge(cvstats,varimpwide,by="SPEC")
+# write.csv(statscombo,file=paste(w,"_statscombo8.csv",sep=""))
